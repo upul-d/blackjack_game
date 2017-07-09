a31d88aa4f7f34e98910d9e22ce3e95e75acf2d3
upuld <u.dissanayaka@gmail.com>
2017-07-09 11:41:12 +0100

    DeckTest: added test for shuffleDeck()

--- blackjack_specs/DeckTest.java
+++ blackjack_specs/DeckTest.java
@@ -17,12 +17,29 @@ public class DeckTest {
   }
 
   @Test
-  public void canGetCardFromDeck()
-  {
+  public void canGetCardFromDeck() {
     assertEquals(Suit.CLUBS, deck.getCard().getSuit());
     assertEquals(Rank.TWO, deck.getCard().getRank());
     assertEquals(2, deck.getCard().getMinValue());
     assertEquals(2, deck.getCard().getMaxValue());
+  }
+
+  @Test
+  public void checkShuffleDeck() {
+    Deck testDeck1 = new Deck();
+    assertEquals(Suit.CLUBS, testDeck1.getCard().getSuit());
+    assertEquals(Rank.TWO, testDeck1.getCard().getRank());
+    assertEquals(2, testDeck1.getCard().getMinValue());
+    assertEquals(2, testDeck1.getCard().getMaxValue());
+
+    Deck testDeck2 = new Deck();
+
+    assertEquals(testDeck1.toString(), testDeck2.toString());
+
+    testDeck1.shuffleDeck();
+    testDeck2.shuffleDeck();
+  
+    assertNotEquals(testDeck1.toString(), testDeck2.toString());
   }
 
 }
