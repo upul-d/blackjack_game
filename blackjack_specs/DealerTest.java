3a13cdacee17c46492bf6556ea8e15fad251eaca
upuld <u.dissanayaka@gmail.com>
2017-07-11 12:58:05 +0100

    Added test for dealer beats player (neither are bust)

--- blackjack_specs/DealerTest.java
+++ blackjack_specs/DealerTest.java
@@ -133,4 +133,18 @@ public class DealerTest {
     assertEquals(PLAYERNAME, dealer.compareScores(testPlayer).getName());
   }
 
+  @Test
+  public void canCompareScoresDealerBeatsPlayerNeitherBust() {
+    Card newDealtCard1 = new Card(Rank.QUEEN,Suit.DIAMONDS,10,10);
+    Card newDealtCard2 = new Card(Rank.ACE,Suit.CLUBS,1,11);
+
+    testPlayer.receiveCard(newDealtCard1.getRank(), newDealtCard1.getSuit(), newDealtCard1.getMinValue(), newDealtCard1.getMaxValue());
+    assertEquals(10, testPlayer.showScore());
+
+    dealer.receiveCard(newDealtCard2.getRank(), newDealtCard2.getSuit(), newDealtCard2.getMinValue(), newDealtCard2.getMaxValue());
+    assertEquals(11, dealer.showScore());
+
+    assertEquals(DEALERNAME, dealer.compareScores(testPlayer).getName());
+  }
+
 }
